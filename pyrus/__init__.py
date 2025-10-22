8494032f36d6095e2c2b6acc93f188f705aa1187
Aelianthe <aelianthe.dev@proton.me>
2025-10-22 13:37:28 +0200

    Re-exporting stuff in init

---
+++ pyrus/__init__.py
@@ -0,0 +1,6 @@
+"""A simple utility lib to bring some interesting functionalities to Python."""
+
+from pyrus.results import Err, Nothing, Ok, Option, Result, Some
+
+
+__all__ = ["Err", "Nothing", "Ok", "Option", "Result", "Some"]
